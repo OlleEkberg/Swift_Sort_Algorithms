9ca705abe7a7a5d63a923b2a150376534e3f23ae
Olle Ekberg <31688577+OlleEkberg@users.noreply.github.com>
2022-01-24 10:25:00 +0100

    Update BubbleSort.swift

--- BubbleSort.swift
+++ BubbleSort.swift
@@ -1,25 +1,27 @@
-func bubbleSort(array: [Int]) -> [Int] {
-    var sortedArray = array
-    sortedArray.reserveCapacity(array.count)
-    let lastCheckIndex = sortedArray.count - 1
+struct BubbleSort {
+    func bubbleSort(array: [Int]) -> [Int] {
+        var sortedArray = array
+        sortedArray.reserveCapacity(array.count)
+        let lastCheckIndex = sortedArray.count - 1
 
-    var swap = true
-    while swap {
-        swap = false
-        
-        for i in 0..<lastCheckIndex {
-            if i == lastCheckIndex {
-                break
-            }
-            let a = sortedArray[i]
-            let b = sortedArray[i + 1]
-            if a > b {
-                sortedArray.swap(i, with: i+1)
-                swap = true
+        var swap = true
+        while swap {
+            swap = false
+            
+            for i in 0..<lastCheckIndex {
+                if i == lastCheckIndex {
+                    break
+                }
+                let a = sortedArray[i]
+                let b = sortedArray[i + 1]
+                if a > b {
+                    sortedArray.swap(i, with: i+1)
+                    swap = true
+                }
             }
         }
+        return sortedArray
     }
-    return sortedArray
 }
 
 /// There exist a function in Swift called `swapAt(_ i: Int, _ j: Int)`
